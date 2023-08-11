60d2a688a90472d31c6f71c600c0f6066a63c9e1
jimmy <jamesmc21998@gmail.com>
2023-08-11 14:21:54 -0500

    add controller for orders

---
+++ app/views/orders/_order.json.jbuilder
@@ -0,0 +1,4 @@
+json.id order.id
+json.clown order.clown.name
+json.passenger order.user.name
+json.rating order.rating
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/clowns" => "clowns#index"
+  get "/orders" => "orders#index"
 end
