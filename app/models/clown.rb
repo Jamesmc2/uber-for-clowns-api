db0b6044beca7610a4d55f66d35aa8b25f1937b0
jimmy <jamesmc21998@gmail.com>
2023-08-11 14:16:43 -0500

    add controller for clowns

--- app/models/clown.rb
+++ app/models/clown.rb
@@ -1,4 +1,12 @@
 class Clown < ApplicationRecord
   has_many :orders
   has_many :users, through: :orders
+
+  def ratings
+    ratings = []
+    orders.each do |order|
+      ratings << order.rating
+    end
+    return ratings
+  end
 end
